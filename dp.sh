35a2742f3670526826c8c35bc7d213ee7e625c61
rain han <samrainhan@gmail.com>
2019-02-16 14:55:01 +0000

    +1

--- dp.sh
+++ dp.sh
@@ -2,7 +2,7 @@
 # snap run hugo -d ../Blog -b "https://www.samrainhan.com/" --cleanDestinationDir
 
 # 快速编译
-snap run hugo -d ../Blog -b "https://www.samrainhan.com/"
+hugo -d ../Blog -b "https://www.samrainhan.com/"
 
 cd ../Blog
 
