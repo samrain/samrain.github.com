7b2992547927931586d8726c5a791f9a797316ea
rain han <samrainhan@gmail.com>
2019-02-16 14:45:47 +0000

    +1

--- preview.sh
+++ preview.sh
@@ -4,4 +4,4 @@ git commit -m '+1'
 
 git push origin master
 
-hugo server --disableFastRender --bind=0.0.0.0 --baseUrl=http://origin-blog-samrain.c9users.io/ -D -F
+hugo server --bind=0.0.0.0 --baseUrl=http://origin-blog-samrain.c9users.io/
