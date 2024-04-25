5e0f7ec7db88e645101c13eef8cd6b580375d2b9
samrain <samrainhan@gmail.com>
2024-04-25 20:38:52 +0800

    +1

--- preview.sh
+++ preview.sh
@@ -4,4 +4,4 @@ git commit -m '+1'
 
 git push origin master
 
-sudo hugo server --bind=0.0.0.0 --port=55555 --disableFastRender
+hugo server --bind=0.0.0.0 --port=55555 --disableFastRender --renderToMemory
